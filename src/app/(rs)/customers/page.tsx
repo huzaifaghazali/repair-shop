bd0c953053f25ce574e66a0a2f49d1dfb2059b61
huzaifaghazali <huzaifaghazali@gmail.com>
2025-04-19 12:09:28 +0500

    Add customer search query functionality

--- src/app/(rs)/customers/page.tsx
+++ src/app/(rs)/customers/page.tsx
@@ -1,7 +1,23 @@
-export const metadata = {
-   title: "Customers",
-}
+import CustomerSearch from './CustomerSearch';
+import { getCustomerSearchResults } from '@/lib/queries/getCustomerSearchResults';
 
-export default function Customers() {
-   return <h2>Customers Page</h2>
+export const metadata = {
+  title: 'Customer Search',
+};
+
+export default async function Customers({
+  searchParams,
+}: {
+  searchParams: Promise<{ [key: string]: string | undefined }>;
+}) {
+  const { searchText } = await searchParams;
+  if (!searchText) return <CustomerSearch />;
+  const results = await getCustomerSearchResults(searchText);
+
+  return (
+    <>
+      <CustomerSearch />
+      <p>{JSON.stringify(results)}</p>
+    </>
+  );
 }
