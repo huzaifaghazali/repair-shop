09bc49de369e45f5e509fbe3db920794a4d33fb8
huzaifaghazali <huzaifaghazali@gmail.com>
2025-04-19 12:16:17 +0500

    Add TicketSearch component

--- src/app/(rs)/tickets/page.tsx
+++ src/app/(rs)/tickets/page.tsx
@@ -1,7 +1,22 @@
-export const metadata = {
-   title: "Tickets",
-}
+import TicketSearch from './TicketSearch';
 
-export default function Tickets() {
-   return <h2>Tickets Page</h2>
+export const metadata = {
+  title: 'Tickets Search',
+};
+
+export default async function Tickets({
+  searchParams,
+}: {
+  searchParams: Promise<{ [key: string]: string | undefined }>;
+}) {
+  const { searchText } = await searchParams;
+
+  if (!searchText) {
+    return (
+      <>
+        <TicketSearch />
+      </>
+    );
+  }
+  return <h2>Tickets Page</h2>;
 }
