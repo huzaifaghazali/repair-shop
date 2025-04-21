7f65c07a654ca8fc44b1b1f2569a5e9363cc03e3
huzaifaghazali <huzaifaghazali@gmail.com>
2025-04-21 16:32:51 +0500

    Add TicketTable component

--- src/app/(rs)/tickets/page.tsx
+++ src/app/(rs)/tickets/page.tsx
@@ -1,6 +1,7 @@
 import TicketSearch from './TicketSearch';
-import { getOpenTickets } from "@/lib/queries/getOpenTickets"
+import { getOpenTickets } from '@/lib/queries/getOpenTickets';
 import { getTicketSearchResults } from '@/lib/queries/getTicketSearchResults';
+import TicketTable from './TicketTable';
 
 export const metadata = {
   title: 'Tickets Search',
@@ -14,11 +15,11 @@ export default async function Tickets({
   const { searchText } = await searchParams;
 
   if (!searchText) {
-   const results = await getOpenTickets()
+    const results = await getOpenTickets();
     return (
       <>
         <TicketSearch />
-        <p>{JSON.stringify(results)}</p>
+        {results.length ? <TicketTable data={results} /> : null}
       </>
     );
   }
@@ -28,7 +29,7 @@ export default async function Tickets({
   return (
     <>
       <TicketSearch />
-      <p>{JSON.stringify(results)}</p>
+      {results.length ? <TicketTable data={results} /> : null}
     </>
   );
 }
